da594f10ac6a5d9a61760884cf0c082bf2b12347
huseynaktas <h.akts1007@gmail.com>
2026-02-03 23:18:19 +0300

    UI improvements and content updates for public pages

---
+++ Frontends/Esnafim_1.WebUI/Controllers/SupportController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Esnafim_1.WebUI.Controllers
+{
+    public class SupportController : Controller
+    {
+        [HttpGet]
+        public IActionResult Terms() => View();
+
+        [HttpGet]
+        public IActionResult Privacy() => View();
+
+        [HttpGet]
+        public IActionResult Faq() => View();
+
+        [HttpGet]
+        public IActionResult HelpCenter() => View();
+
+        [HttpGet]
+        public IActionResult Feedback() => View();
+    }
+}
--- Frontends/Esnafim_1.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
+++ Frontends/Esnafim_1.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Esnafim_1.Dto.FooterAddressDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace Esnafim_1.WebUI.ViewComponents.UILayoutViewComponents
 {
     public class _FooterUILayoutComponentPartial: ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FooterUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var apiConnectionLink = "https://localhost:7028/api/FooterAddresses";
+            var responseMesage = await client.GetAsync(apiConnectionLink);
+            if (responseMesage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMesage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFooterAddressDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
